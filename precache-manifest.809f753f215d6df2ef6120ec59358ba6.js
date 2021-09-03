d8be4146c4d35fcd173c3729bb4d2964c18b7d9e
James Newton <blackslate@lexogram.com>
2021-09-03 22:00:16 +0300

    Updates

---
+++ precache-manifest.809f753f215d6df2ef6120ec59358ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3ca13d7d5957b9e535ebf9514ff901",
+    "url": "/lobster/index.html"
+  },
+  {
+    "revision": "f82150c4fdfea2e1385d",
+    "url": "/lobster/static/css/main.0a645fef.chunk.css"
+  },
+  {
+    "revision": "af4994e25477c90a46cd",
+    "url": "/lobster/static/js/2.5434e56f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/lobster/static/js/2.5434e56f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f82150c4fdfea2e1385d",
+    "url": "/lobster/static/js/main.e4510a09.chunk.js"
+  },
+  {
+    "revision": "b17ff2c94eac7fdc236a",
+    "url": "/lobster/static/js/runtime-main.47d6a58e.js"
+  }
+]);
